2a403f607623b03c2ce065098e9060f26928e722
Soohan Bae <46469648+shawnbae@users.noreply.github.com>
2022-03-11 15:37:04 +0900

    Time: 2500 ms (50.82%), Space: 29.8 MB (70.54%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -5,18 +5,15 @@ class Solution:
         res = []
         for i, cur in enumerate(nums[:k-1]):
             while q and nums[q[-1]]<cur: q.pop()
-            q.append(i) # Store index 
+            q.append(i)
         
         for i in range(k-1,len(nums)):
             cur=nums[i]
-            # Pop elements from tail if tail elements are smaller than cur
+            
             while q and nums[q[-1]]<cur: q.pop()
             q.append(i)
             
-            res.append(nums[q[0]]) # Max stored at Head
-            
-            # Since queue is in sorted index low->high
-            # If head is == indToRemove(rem) part then popleft
+            res.append(nums[q[0]])
             rem=i-k+1
             if q[0]==rem:  q.popleft()
             
